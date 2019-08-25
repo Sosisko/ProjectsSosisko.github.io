477427e161ac94558c683e19ff20dc6306ae6611
Alex <svetlampy@mail.ru>
2019-08-25 19:57:03 +0600

    first commit

---
+++ 33s/js/javascript.js
@@ -0,0 +1,30 @@
+﻿$(function(){
+	$(".accordeon > li > a").click(function (e) {
+	e.preventDefault();
+	
+	var menu = $(this).closest('.accordeon');
+	
+	if (false == $(this).next().is(':visible')) {
+		menu.find('li').removeClass('slide active');
+		menu.find('ul').slideUp();
+	}
+	
+	$(this).next().slideToggle();
+	$(this).parent().addClass('slide');
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+# Projects-Sosisko
+# Projects-Sosisko
